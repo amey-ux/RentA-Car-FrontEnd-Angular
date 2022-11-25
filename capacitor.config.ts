fe1db254f5ef8f6dd3b0c64dd5e1aa4307516dbd
amey-ux <amey.singh@yanitsolutions.com>
2022-11-25 14:59:48 +0530

    capc config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'Trax.Flagship.Inspect',
   appName: 'rent-acar-front-end',
   webDir: 'www',
   bundledWebRuntime: false
