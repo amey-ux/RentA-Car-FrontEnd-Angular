74fb2c1b5a76d419e09632e178e81a0b1dff7ee2
amey-ux <amey.singh@yanitsolutions.com>
2022-11-29 13:08:17 +0530

    changed app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'Trax.Flagship.Inspect',
-  appName: 'rent-acar-front-end',
+  appName: 'Flagship.Inspect',
   webDir: 'www',
   bundledWebRuntime: false
 };
